5430f490ac0090fbf02e542196665f6ca58f5c84
lomichelotti <michelotti2k@gmail.com>
2025-03-08 02:25:33 -0300

    floating numbers last for longer

--- floating_text/floating_text.gd
+++ floating_text/floating_text.gd
@@ -6,7 +6,7 @@ func animate():
 	show()
 	var tween = create_tween().bind_node(self)
 	modulate.a = 1
-	tween.tween_property(self, "position:y", position.y - 8, .5)
+	tween.tween_property(self, "position:y", position.y - 8, 1)
 	tween.tween_property(self, "position:y", position.y + 8, .2)
 	tween.parallel().tween_property(self, "modulate:a", 0, .2)
 	
